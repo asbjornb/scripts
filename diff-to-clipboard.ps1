a94e15db5333d3247f46e6f0c83137663b7b6e79
Asbjørn Brandt <abr@norlysenergytrading.com>
2025-08-07 13:31:54 +0200

    Avoid hardcoding main branch name

--- diff-to-clipboard.ps1
+++ diff-to-clipboard.ps1
@@ -1,3 +1,6 @@
-$base = git merge-base main HEAD
+$defaultBranch = git symbolic-ref refs/remotes/origin/HEAD 2>$null | Split-Path -Leaf
+if (-not $defaultBranch) { $defaultBranch = "main" }
+
+$base = git merge-base $defaultBranch HEAD
 git diff $base HEAD | Set-Clipboard
 Write-Output "Copied diff from $base to HEAD to clipboard."
